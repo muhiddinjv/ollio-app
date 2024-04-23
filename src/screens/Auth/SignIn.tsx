e7c46b7ff61dc69ef65190025ee4458c215d8d1e
muhiddin <muhiddinjv@gmail.com>
2024-04-23 15:09:27 +0500

    singin logic done

--- src/screens/Auth/SignIn.tsx
+++ src/screens/Auth/SignIn.tsx
@@ -3,7 +3,6 @@ import { Text, View, Button } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { TextInput, useTheme, Button as RNButton } from 'react-native-paper';
 import { INavigation } from '../../utils/interfaces';
-import { signIn } from '../../api/requests';
 import { AuthProvider, useAuth } from '.';
 import { setToken } from './astorage';
 import axios from 'axios';
@@ -21,23 +20,23 @@ const LogInButton = () => {
 export default function SignIn({ navigation }: INavigation) {
   const [phoneNumber, setPhoneNumber] = React.useState("");
   const [password, setPassword] = React.useState("");
-  const [error, setError] = React.useState<any>(); // Keep using the array of errors
+  const [showPassword, setShowPassword] = React.useState(false);
+  const [error, setError] = React.useState<any[]>();
   const { status, userToken } = useAuth();
   const theme = useTheme();
 
   const handleSignIn = async () => {
     try {
       const response = await axios.post('http://10.0.2.2:4000/auth/signin', { phoneNumber, password });
-      setError([]); // Clear errors on successful response
-      setToken(response.data.token);
+      setToken(response.data.accessToken);
       navigation.navigate('Sales');
       return response.data;
     } catch (error: any) {
       setError(error?.response?.data?.message || []);
+      setTimeout(() => setError([]), 4000);
     }
   };
 
-  // Function to find an error for a specific field
   const findErrorForField = (fieldName: string) => {
     const fieldError = error?.find((err: any) => err.field === fieldName);
     return fieldError ? fieldError.text : null;
@@ -47,12 +46,14 @@ export default function SignIn({ navigation }: INavigation) {
     <AuthProvider>
       <View className='flex-1 justify-center bg-gray-200 p-2'>
         <SafeAreaView className="flex flex-grow justify-center p-16 dark:bg-slate-800">
+          <LogOutButton />
           <View className="flex flex-1 items-center justify-center">
             {findErrorForField("phoneNumber") && (
               <Text className="text-red-500">{findErrorForField("phoneNumber")}</Text>
             )}
             <TextInput
               label="Phone"
+              error={findErrorForField("phoneNumber") ? true : false}
               value={phoneNumber}
               keyboardType="number-pad"
               onChangeText={(text) => setPhoneNumber(text)}
@@ -65,12 +66,14 @@ export default function SignIn({ navigation }: INavigation) {
             )}
             <TextInput
               label="Password"
+              error={findErrorForField("password") ? true : false}
               value={password}
               onChangeText={(text) => setPassword(text)}
-              secureTextEntry
+              secureTextEntry={!showPassword}
               mode="outlined"
               textColor={theme.colors.secondary}
               className="mb-5 w-full dark:bg-slate-700"
+              right={<TextInput.Icon icon="eye" onPress={() => setShowPassword(!showPassword)} />}
             />
             <RNButton mode="contained" onPress={handleSignIn} className="mb-5 w-full p-1" textColor="white">
               Sign In

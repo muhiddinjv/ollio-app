9b2d40928726ea0ecd654312e1413301d7825766
islomaliy <islomaliy74@gmail.com>
2024-07-13 17:22:46 +0500

    added: bills list and details page

---
+++ src/screens/Bills/BillDetails.js
@@ -0,0 +1,120 @@
+import React from "react";
+import { FlatList, Pressable, ScrollView, View } from "react-native";
+import { Text } from "react-native";
+import Loader from "../../components/Loader";
+
+const Item = ({ name, quantity, price }) => {
+  return (
+    <View>
+      <Text>{name}</Text>
+      <Text>x {quantity}</Text>
+      <Text>{price} UZS</Text>
+    </View>
+  );
+};
+const BillDetails = () => {
+  const items = [
+    { name: "a74 cola 1.5L (1bl*6ta)", quantity: 1, price: "15,000", id: 0 },
+    { name: "a75 cola 1.5L (1bl*6ta)", quantity: 2, price: "32,000", id: 1 },
+    { name: "a76 fanta 1.5L (1bl*6ta)", quantity: 3, price: "51,000", id: 2 },
+    { name: "a77 sprite 1.5L (1bl*6ta)", quantity: 4, price: "70,000", id: 3 },
+    { name: "a78 pepsi 1.5L (1bl*6ta)", quantity: 5, price: "89,000", id: 4 },
+    {
+      name: "a79 redbull 1.5L (1bl*6ta)",
+      quantity: 6,
+      price: "108,000",
+      id: 5,
+    },
+    { name: "a80 water 1.5L (1bl*6ta)", quantity: 7, price: "127,000", id: 6 },
+    { name: "a81 water 1.5L (1bl*6ta)", quantity: 8, price: "146,000", id: 7 },
+    { name: "a82 water 1.5L (1bl*6ta)", quantity: 9, price: "165,000", id: 8 },
+    { name: "a83 water 1.5L (1bl*6ta)", quantity: 10, price: "184,000", id: 9 },
+    {
+      name: "a84 water 1.5L (1bl*6ta)",
+      quantity: 11,
+      price: "203,000",
+      id: 10,
+    },
+    {
+      name: "a85 water 1.5L (1bl*6ta)",
+      quantity: 12,
+      price: "222,000",
+      id: 11,
+    },
+    {
+      name: "a86 water 1.5L (1bl*6ta)",
+      quantity: 13,
+      price: "241,000",
+      id: 12,
+    },
+  ];
+
+  return (
+    <ScrollView className="p-4">
+      <View className="py-4 border-b border-gray-300">
+        <Text className="text-4xl text-center font-medium">UZS 150 000</Text>
+        <Text className="text-gray-600 text-xl text-center">Total</Text>
+      </View>
+      <View className="py-4 border-b border-gray-300">
+        <Text className="text-lg">Employee: muhidding001@gmail.com</Text>
+        <Text className="text-lg">POS: Mega planet POS</Text>
+        <Text className="text-lg">Customer: Bill Gates</Text>
+      </View>
+
+      <View className="py-4 border-b border-gray-300">
+        {items.map((item) => (
+          <View key={item.id} className="flex-row justify-between py-4">
+            <View className="flex-row items-center gap-8">
+              <View>
+                <Text className="text-lg">UZS {item.name}</Text>
+                <Text className="text-gray-500">
+                  {item.quantity} x {item.price}
+                </Text>
+              </View>
+            </View>
+            <Text className="text-gray-500 text-lg">#{item.price}</Text>
+          </View>
+        ))}
+      </View>
+      <View className="flex-row justify-between items-center py-4 border-b border-gray-300">
+        <Text className="font-semibold text-xl">Total:</Text>
+        <Text className="font-semibold text-lg">UZS 150 000</Text>
+      </View>
+      <View className="flex-row items-center justify-between py-4">
+        <Text className="text-lg text-gray-500">12/11/23 10:44</Text>
+        <Text className="text-lg text-gray-500">#2-1001</Text>
+      </View>
+      <View className="pb-24">
+        <Pressable className="py-3 bg-primary rounded">
+          <Text className="text-xl text-center text-white">Download</Text>
+        </Pressable>
+      </View>
+      {/* <FlatList
+        data={items}
+        onEndReachedThreshold={2}
+        removeClippedSubviews={true}
+        estimatedItemSize={84}
+        renderItem={({ item }) => (
+          <View className="flex-row justify-between border-b border-gray-200 py-4">
+            <View className="flex-row items-center gap-8">
+              <View>
+                <Text className="text-lg">UZS {item.price}</Text>
+                <Text className="text-gray-500">{item.time}</Text>
+              </View>
+            </View>
+            <Text className="text-gray-500 text-lg">#{item.id}</Text>
+          </View>
+        )}
+        keyExtractor={(item) => item.id.toString()}
+        LoaderComponent={<Loader />}
+        ListEmptyComponent={
+          <View className="flex items-center">
+            <Loader />
+          </View>
+        }
+      /> */}
+    </ScrollView>
+  );
+};
+
+export default BillDetails;
--- src/screens/Bills/Bills.js
+++ src/screens/Bills/Bills.js
@@ -1,44 +1,120 @@
-import { View } from 'react-native'
-import { Text } from 'react-native-paper'
-import React, { useContext } from 'react'
-import { useQuery } from '@tanstack/react-query'
-import { getAccessToken } from '../Auth/astorage'
-import axiosInstance from '../../api/instance'
-import { GlobalContext } from '../../utils'
+import { Pressable, View } from "react-native";
+import { Text } from "react-native-paper";
+import React, { useContext } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { getAccessToken } from "../Auth/astorage";
+import axiosInstance from "../../api/instance";
+import { GlobalContext } from "../../utils";
+import { FlashList } from "@shopify/flash-list";
+import Loader from "../../components/Loader";
+import { FontAwesome6 } from "@expo/vector-icons";
+const billsData = [
+  {
+    price: "3000",
+    time: "21:04",
+    id: 1,
+  },
+  {
+    price: "4000",
+    time: "10:04",
+    id: 2,
+  },
+  {
+    price: "5000",
+    time: "11:04",
+    id: 3,
+  },
+  {
+    price: "8000",
+    time: "19:04",
+    id: 4,
+  },
+  {
+    price: "9000",
+    time: "20:04",
+    id: 5,
+  },
+  {
+    price: "10000",
+    time: "21:04",
+    id: 6,
+  },
+  {
+    price: "20000",
+    time: "22:04",
+    id: 7,
+  },
+];
 
-const Bills = () => {
-    const { goodId, goodQty } = useContext(GlobalContext);
+const Bills = ({ navigation }) => {
+  const { goodId, goodQty } = useContext(GlobalContext);
 
-    const billsQuery = useQuery({
-        queryKey: ["bills"], queryFn: async () => {
-            const accessToken = await getAccessToken()
-            const response = await axiosInstance.post("bills", 
+  const billsQuery = useQuery({
+    queryKey: ["bills"],
+    queryFn: async () => {
+      const accessToken = await getAccessToken();
+      const response = await axiosInstance.post(
+        "bills",
+        {
+          buyer: "66366f44d06d804edde49493",
+          goods: [
             {
-                buyer:"66366f44d06d804edde49493",
-                goods: [
-                    {
-                        _id: "665eee37cd18d7e147b8baaf",
-                        quantity: 3
-                    },
-                    {
-                        _id: "665eee37cd18d7e147b8bab2",
-                        quantity: 4
-                    }
-                ]
+              _id: "665eee37cd18d7e147b8baaf",
+              quantity: 3,
             },
-            { headers: { Authorization: `Bearer ${accessToken}` }})
-            
-            return response.data
-        }
-    })
+            {
+              _id: "665eee37cd18d7e147b8bab2",
+              quantity: 4,
+            },
+          ],
+        },
+        { headers: { Authorization: `Bearer ${accessToken}` } }
+      );
+
+      return response.data;
+    },
+  });
 
   return (
-    <View className='p-4'>
-      <Text>title + quantity</Text>
+    <View className="p-4 flex-1">
+      <View className="border-b border-gray-200 pb-4">
+        <Text className="text-primary font-medium text-lg">
+          Wednesday, December 13, 2024
+        </Text>
+      </View>
+      <FlashList
+        data={billsData}
+        onEndReachedThreshold={2}
+        removeClippedSubviews={true}
+        estimatedItemSize={84}
+        renderItem={({ item }) => (
+          <Pressable
+            className="flex-row justify-between border-b border-gray-200 py-4"
+            onPress={() => navigation.navigate("BillDetails")}
+          >
+            <View className="flex-row items-center gap-8">
+              <FontAwesome6 name="money-bills" size={24} color="black" />
+              <View>
+                <Text className="text-lg">UZS {item.price}</Text>
+                <Text className="text-gray-500">{item.time}</Text>
+              </View>
+            </View>
+            <Text className="text-gray-500 text-lg">#{item.id}</Text>
+          </Pressable>
+        )}
+        keyExtractor={(item) => item.id.toString()}
+        LoaderComponent={<Loader />}
+        ListEmptyComponent={
+          <View className="flex items-center">
+            <Loader />
+          </View>
+        }
+      />
+      {/* <Text>title + quantity</Text>
       <Text>total price per product</Text>
-      <Text>total price of all products</Text>
+      <Text>total price of all products</Text> */}
     </View>
-  )
-}
+  );
+};
 
-export default Bills
+export default Bills;

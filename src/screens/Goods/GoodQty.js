cbd405b0ebbbcf8d238c9b021bf4802627804059
islomaliy <islomaliy74@gmail.com>
2024-07-13 15:28:41 +0500

    prettified

--- src/screens/Goods/GoodQty.js
+++ src/screens/Goods/GoodQty.js
@@ -1,44 +1,54 @@
-import { View, Platform } from 'react-native'
-import { useContext, useEffect, useState } from 'react'
-import { useQuery } from '@tanstack/react-query';
-import { getAccessToken } from '../Auth/astorage';
-import axiosInstance from '../../api/instance';
-import { GlobalContext } from '../../utils';
-import { TextInput } from 'react-native-paper';
-import Header from '../../components/Header';
+import { View, Platform } from "react-native";
+import { useContext, useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { getAccessToken } from "../Auth/astorage";
+import axiosInstance from "../../api/instance";
+import { GlobalContext } from "../../utils";
+import { TextInput } from "react-native-paper";
+import Header from "../../components/Header";
 
 const GoodQty = ({ navigation }) => {
-    const { goodId, setGoodQty, goodQty } = useContext(GlobalContext);
-    const [title, setTitle] = useState();
+  const { goodId, setGoodQty, goodQty } = useContext(GlobalContext);
+  const [title, setTitle] = useState();
 
-    const goodsQuery = useQuery({
-        queryKey: ["good", goodId], queryFn: async () => {
-            const accessToken = await getAccessToken()
-            const response = await axiosInstance.get(`goods/${goodId}`, {
-                headers: { Authorization: `Bearer ${accessToken}` }
-            })
-            return response.data
-        }
-    })
+  const goodsQuery = useQuery({
+    queryKey: ["good", goodId],
+    queryFn: async () => {
+      const accessToken = await getAccessToken();
+      const response = await axiosInstance.get(`goods/${goodId}`, {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      });
+      return response.data;
+    },
+  });
 
-    useEffect(() => {
-        if (goodsQuery?.data) {
-            setTitle(goodsQuery?.data.title);
-        }
-    }, [goodsQuery?.data, goodId]);
+  useEffect(() => {
+    if (goodsQuery?.data) {
+      setTitle(goodsQuery?.data.title);
+    }
+  }, [goodsQuery?.data, goodId]);
 
-    return (
-        <View>
-            <Header title={title} fontSize={18} icon="content-save" navigation={navigation} backBtn />
-            <TextInput 
-                onChangeText={qty => setGoodQty(qty)} 
-                value={goodQty} returnKeyType='done' 
-                onSubmitEditing={() => navigation.navigate('Sales')} 
-                keyboardType={Platform.OS === 'ios' ? "number-pad" : "numeric"} 
-                placeholder='0' className='text-2xl' autoFocus 
-            />
-        </View>
-    )
-}
+  return (
+    <View>
+      <Header
+        title={title}
+        fontSize={18}
+        icon="content-save"
+        navigation={navigation}
+        backBtn
+      />
+      <TextInput
+        onChangeText={(qty) => setGoodQty(qty)}
+        value={goodQty}
+        returnKeyType="done"
+        onSubmitEditing={() => navigation.navigate("Sales")}
+        keyboardType={Platform.OS === "ios" ? "number-pad" : "numeric"}
+        placeholder="0"
+        className="text-2xl"
+        autoFocus
+      />
+    </View>
+  );
+};
 
-export default GoodQty
+export default GoodQty;
